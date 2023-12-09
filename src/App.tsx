b344fa271865871e52168510e462ef6bfd3d3f01
David Nanculeo <dnanculeob@uft.edu>
2023-12-09 19:37:24 -0300

    Plataforma Proyecto Web Mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App: React.FC = () => {
+  const [data, setData] = useState([]);
+  const [formData, setFormData] = useState({
+    nombre: '',
+    edad: '',
+    fechaNacimiento: '',
+  });
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/data');
+      if (response.ok) {
+        const result = await response.json();
+        setData(result);
+      } else {
+        console.error('Error al obtener datos del servidor');
+      }
+    } catch (error) {
+      console.error('Error al obtener datos:', error);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/api/addData', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log('Datos enviados correctamente');
+        // Actualizar la lista de datos después de enviar los nuevos datos
+        fetchData();
+      } else {
+        console.error('Error al enviar datos al servidor');
+      }
+    } catch (error) {
+      console.error('Error al enviar datos al servidor:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ingreso de Datos</h1>
+
+      {/* Formulario para ingresar datos */}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nombre:
+          <input
+            type="text"
+            name="nombre"
+            value={formData.nombre}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Edad:
+          <input
+            type="text"
+            name="edad"
+            value={formData.edad}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Fecha de Nacimiento:
+          <input
+            type="text"
+            name="fechaNacimiento"
+            value={formData.fechaNacimiento}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Enviar Datos</button>
+      </form>
+
+      {/* Mostrar datos existentes */}
+      <ul>
+        {data.map((item: any) => (
+          <li key={item.id}>
+            {item.nombre} - {item.edad} años - {item.fechaNacimiento}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/server.js
@@ -0,0 +1,50 @@
+const cors = require('cors');
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+// Configurar la conexión a MySQL
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456789',
+  database: 'ProyectoWebMobile',
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Error de conexión a MySQL:', err);
+  } else {
+    console.log('Conexión a MySQL exitosa');
+  }
+});
+
+// Middleware CORS (colocado antes de las rutas)
+app.use(cors());
+
+// Endpoint de ejemplo para obtener datos de MySQL
+app.get('/api/data', (req, res) => {
+  db.query('SELECT * FROM personas', (err, result) => {
+    if (err) {
+      console.error('Error al consultar MySQL:', err);
+      res.status(500).json({ error: 'Error al consultar MySQL' });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// Sirve archivos estáticos desde la carpeta 'public' (o ajusta según tu estructura)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configuración para manejar rutas no definidas (debe estar al final)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Node.js en http://localhost:${port}`);
+});
